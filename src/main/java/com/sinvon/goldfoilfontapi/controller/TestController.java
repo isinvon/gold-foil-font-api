2ff28377fac0dd68e634e2158c9a76abcda78778
isinvon <2021469084@qq.com>
2024-12-09 01:41:25 +0800

    refactor(controller): 移除测试接口中的打印语句, 移除未使用的导入
    - 删除了 TestController 类中 test 方法的 System.out.println 语句
    - 优化了代码结构，提高了代码的可读性和维护性

--- src/main/java/com/sinvon/goldfoilfontapi/controller/TestController.java
+++ src/main/java/com/sinvon/goldfoilfontapi/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.sinvon.goldfoilfontapi.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ public class TestController {
 
     @GetMapping("1")
     public String test() {
-        System.out.println("http://localhost:8080/test/1");
         return "test";
     }
     @GetMapping("2")
