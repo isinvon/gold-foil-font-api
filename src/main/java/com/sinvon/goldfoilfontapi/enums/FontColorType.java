9c4b58ac2a846ad4b0a076faa1e109d9829682e0
isinvon <2021469084@qq.com>
2024-12-09 14:14:08 +0800

    feat: 添加字体颜色类型类

---
+++ src/main/java/com/sinvon/goldfoilfontapi/enums/FontColorType.java
@@ -0,0 +1,25 @@
+package com.sinvon.goldfoilfontapi.enums;
+
+/**
+ * 字体颜色类型
+ *
+ * @author : sinvon
+ * @since :  2024/12/9 下午2:11
+ */
+public class FontColorType {
+
+    /**
+     * 金色
+     */
+    public static final String GOLD = "gold";
+
+    /**
+     * 银色
+     */
+    public static final String SILVER = "silver";
+
+    /**
+     * 黑色
+     */
+    public static final String BLACK = "black";
+}
