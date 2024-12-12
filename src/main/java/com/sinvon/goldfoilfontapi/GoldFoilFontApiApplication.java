fef9af0d8d7b142e20f51b0b7db9ff54424f52da
isinvon <2021469084@qq.com>
2024-12-12 23:46:32 +0800

    feat: 添加浏览器工具类并实现自动打开浏览器功能
    - 新增 BrowserUtils 工具类，用于打开浏览器
    - 修改 GoldFoilFontApiApplication 主类，使用 BrowserUtils 自动打开浏览器访问首页- 移除了 WebviewLauncher相关代码，简化了应用启动逻辑

--- src/main/java/com/sinvon/goldfoilfontapi/GoldFoilFontApiApplication.java
+++ src/main/java/com/sinvon/goldfoilfontapi/GoldFoilFontApiApplication.java
@@ -1,20 +1,20 @@
 package com.sinvon.goldfoilfontapi;
 
-import com.sinvon.goldfoilfontapi.webview.WebviewLauncher;
+import com.sinvon.goldfoilfontapi.utils.BrowserUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author : sinvon
  * @since : 2024年12月9日12:30:27
  */
 @SpringBootApplication
+@EnableScheduling
 public class GoldFoilFontApiApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(GoldFoilFontApiApplication.class, args);
-        WebviewLauncher webviewLauncher = context.getBean(WebviewLauncher.class);
-        webviewLauncher.launchWebview();
+        SpringApplication.run(GoldFoilFontApiApplication.class, args);
+        BrowserUtils.openBrowser("http://localhost:8080/index.html");
     }
 }
