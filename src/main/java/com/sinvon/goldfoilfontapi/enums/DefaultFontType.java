c4cd6cd4cb9d3e45d583cbeedfe20d12aa5235a9
isinvon <2021469084@qq.com>
2024-12-14 15:43:11 +0800

    fix(font): 修正应用默认字体类型名称
    - 将 "三级泼墨体" 修改为 "三极泼墨体"

--- src/main/java/com/sinvon/goldfoilfontapi/enums/DefaultFontType.java
+++ src/main/java/com/sinvon/goldfoilfontapi/enums/DefaultFontType.java
@@ -7,5 +7,5 @@ package com.sinvon.goldfoilfontapi.enums;
  * @since :  2024/12/14 下午3:32
  */
 public class DefaultFontType {
-    public static final String APP_DEFAULT_FONT_TYPE = "三级泼墨体";
+    public static final String APP_DEFAULT_FONT_TYPE = "三极泼墨体";
 }
