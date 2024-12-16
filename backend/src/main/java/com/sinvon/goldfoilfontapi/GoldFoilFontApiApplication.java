0e7fbd883ef743dc5cbcbed806006d3f59c60ed5
isinvon <2021469084@qq.com>
2024-12-16 23:30:38 +0800

    refactor: 移除未使用的定时任务注解

--- backend/src/main/java/com/sinvon/goldfoilfontapi/GoldFoilFontApiApplication.java
+++ backend/src/main/java/com/sinvon/goldfoilfontapi/GoldFoilFontApiApplication.java
@@ -2,14 +2,12 @@ package com.sinvon.goldfoilfontapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author : sinvon
  * @since : 2024年12月9日12:30:27
  */
 @SpringBootApplication
-@EnableScheduling
 public class GoldFoilFontApiApplication {
 
     public static void main(String[] args) {
