7d2f420450b8ec2d871133000cd81b4d214bd15d
isinvon <2021469084@qq.com>
2024-12-19 22:34:44 +0800

    refactor(frontend): 移除心跳包功能
    - 删除了 sendHeartbeat.js 文件
    - 从 main.js 中移除了心跳包相关的引用和调用

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,5 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './styles/index.less'
-import {sendHeartbeat} from "@/api/sendHeartbeat.js";
-
-// 在应用挂载前调用 sendHeartbeat 来监听可见性变化
-sendHeartbeat();
 
 createApp(App).use(ElementPlus).mount('#app')
